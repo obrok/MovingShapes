1aaa9c70c70d61ef8049e63f047bb6af9f2381f0
obrok <pawel.obrok@gmail.com>
2012-03-22 19:02:58 +0100

    Draw a polygon

--- src/pl/hackkrk/shapes/activity/MainActivity.java
+++ src/pl/hackkrk/shapes/activity/MainActivity.java
@@ -1,16 +1,27 @@
 package pl.hackkrk.shapes.activity;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import pl.hackkrk.shapes.R;
+import pl.hackkrk.shapes.view.ShapeView;
 
 public class MainActivity extends Activity
 {
+    ShapeView shapeView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        shapeView = (ShapeView) findViewById(R.id.shape_view);
+        shapeView.setPolygon(
+                new Point(10, 20),
+                new Point(100, 120),
+                new Point(10, 120)
+        );
     }
 }
--- src/pl/hackkrk/shapes/view/ShapeView.java
+++ src/pl/hackkrk/shapes/view/ShapeView.java
@@ -1,14 +1,13 @@
 package pl.hackkrk.shapes.view;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ShapeView extends View {
     Paint paint = new Paint();
+    Path path;
 
     public ShapeView(Context context) {
         super(context);
@@ -22,9 +21,24 @@ public class ShapeView extends View {
         super(context, attrs, defStyle);
     }
 
+    public void setPolygon(Point... locations){
+        path = new Path();
+        Point first = locations[0];
+        path.moveTo(first.x, first.y);
+        for (int i = 1; i < locations.length; i++) {
+            Point location = locations[i];
+            path.lineTo(location.x, location.y);
+        }
+
+        invalidate();
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         paint.setColor(Color.WHITE);
-        canvas.drawLine(0, 0, 10, 10, paint);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.argb(255, 0, 123, 242));
+
+        canvas.drawPath(path, paint);
     }
 }
